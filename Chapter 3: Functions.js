add9b7ebacd5d3b7b87add7bc23ec2c46a4f5802
hunjager <59488679+hunjager@users.noreply.github.com>
2023-06-12 13:53:56 +0100

    Create Chapter 3: Functions.js

---
+++ Chapter 3: Functions.js
@@ -0,0 +1,24 @@
+/*Write a function
+min that takes two arguments and returns their minimum.*/
+function min(a,b){
+    console.log(Math.min(a,b));
+}
+
+
+
+
+/*Define a recursive function isEven corresponding to this description. The
+function should accept a single parameter (a positive, whole number) and return
+a Boolean. */
+function isEven(num){
+    if(num%2==0){
+        console.log(true);
+        console.log("The number " + num + " is even");
+    }
+    else{
+        console.log(false);
+        console.log("The number " + num + " is not even");
+
+    }
+}
+
