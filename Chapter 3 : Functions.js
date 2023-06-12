867d603b3479267c47cfd30b58fb688db65cf925
hunjager <59488679+hunjager@users.noreply.github.com>
2023-06-12 13:58:05 +0100

    Update and rename Chapter 3: Functions.js to Chapter 3 : Functions.js

--- Chapter 3 : Functions.js
+++ Chapter 3 : Functions.js
@@ -6,7 +6,6 @@ function min(a,b){
 
 
 
-
 /*Define a recursive function isEven corresponding to this description. The
 function should accept a single parameter (a positive, whole number) and return
 a Boolean. */
